1d067bd739804e3082754095f74ebaac8b099600
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-07-06 21:02:30 +0300

    git commit -m "style: add Inter font"

--- src/app/(shop)/layout.js
+++ src/app/(shop)/layout.js
@@ -1,13 +1,22 @@
 import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
   title: "griffin",
+  description: "Modern Clothing Store",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`min-h-full flex flex-col ${inter.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
