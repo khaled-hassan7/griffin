bbdf9f6a6c3465323ea22661d9187f6101da2f95
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-07-27 01:21:53 +0300

    refactor(button): improve button component and add-to-cart button

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
 
-function Button({ href, children, className }) {
+function Button({ href, children, className, onClick }) {
   return (
     <Link
+      onClick={onClick}
       href={href}
-      className={`text-xs border-text-primary border text-text-primary py-2 font-bold  px-6 hover:bg-primary hover:text-white tracking-widest uppercase   `}
+      className={`flex justify-center text-xs border-text-primary border text-text-primary py-2 font-bold text-center px-6 hover:bg-primary hover:text-white tracking-widest uppercase ${className}   `}
     >
       {children}
     </Link>
